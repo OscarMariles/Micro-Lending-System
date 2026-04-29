3647945cc6bad17727c178ef99ed459f80d5d141
Oscar Alejandro Altamirano Mariles <oaltamirano64@gmail.com>
2026-04-29 12:05:21 -0600

    Delete Autowired/Autowired directory

--- Autowired/Autowired/src/main/java/com/SpringBoot/Autowired/repository/IPosteoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SpringBoot.Autowired.repository;
-
-import com.SpringBoot.Autowired.model.Posteo;
-import java.util.List;
-
-public interface IPosteoRepository {
-    public List<Posteo> traerTodos();
-}
