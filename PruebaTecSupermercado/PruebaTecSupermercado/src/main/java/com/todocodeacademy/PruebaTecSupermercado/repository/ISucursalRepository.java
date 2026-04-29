ca740404e115f205477e931f552b8d9c4ced23b4
Oscar Alejandro Altamirano Mariles <oaltamirano64@gmail.com>
2026-04-29 12:08:36 -0600

    Delete PruebaTecSupermercado directory

--- PruebaTecSupermercado/PruebaTecSupermercado/src/main/java/com/todocodeacademy/PruebaTecSupermercado/repository/ISucursalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todocodeacademy.PruebaTecSupermercado.repository;
-
-import com.todocodeacademy.PruebaTecSupermercado.model.Sucursal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ISucursalRepository extends JpaRepository<Sucursal,Long> {
-}
--- PruebaTecSupermercado/PruebaTecSupermercado/src/main/java/com/todocodeacademy/PruebaTecSupermercado/repository/IVentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todocodeacademy.PruebaTecSupermercado.repository;
-
-import com.todocodeacademy.PruebaTecSupermercado.model.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IVentaRepository extends JpaRepository<Venta,Long> {
-}
