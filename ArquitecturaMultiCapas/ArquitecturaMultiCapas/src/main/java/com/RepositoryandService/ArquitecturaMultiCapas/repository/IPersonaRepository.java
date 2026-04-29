54e9f17d585c323c0398ea9a28f43f7247d78d41
Oscar Alejandro Altamirano Mariles <oaltamirano64@gmail.com>
2026-04-29 12:05:07 -0600

    Delete ArquitecturaMultiCapas/ArquitecturaMultiCapas directory

--- ArquitecturaMultiCapas/ArquitecturaMultiCapas/src/main/java/com/RepositoryandService/ArquitecturaMultiCapas/repository/IPersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.RepositoryandService.ArquitecturaMultiCapas.repository;
-
-import com.RepositoryandService.ArquitecturaMultiCapas.model.Persona;
-
-public interface IPersonaRepository {
-   public void guardarPersons(Persona per);
-   public Persona getPersona();
-}
--- ArquitecturaMultiCapas/ArquitecturaMultiCapas/src/main/java/com/RepositoryandService/ArquitecturaMultiCapas/service/IPersonaService.java
+++
@@ -1,9 +0,0 @@
-package com.RepositoryandService.ArquitecturaMultiCapas.service;
-
-import com.RepositoryandService.ArquitecturaMultiCapas.model.Persona;
-import java.util.List;
-
-public interface IPersonaService {
-    public void crearPersona(Persona per);
-    public List<Persona> getPersonas();
-}
